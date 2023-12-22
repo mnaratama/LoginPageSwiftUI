4b9ff90263ebc460d0506ab09375dd053d7bdf9a
mnaratama <mnaratama@gmail.com>
2023-12-22 15:29:11 +0700

    Initial Commit

---
+++ LoginPageSwiftUI/LoginPageSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageSwiftUIApp.swift
+//  LoginPageSwiftUI
+//
+//  Created by User01 on 22/12/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
